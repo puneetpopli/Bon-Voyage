bd511b9770b22a1e902b2d2fedae1a62bf754ee5
Puneet Popli <puneetpopli.26pp@gmail.com>
2015-12-01 16:34:50 -0800

    Navbar bug fixed by Tushar

--- src/main/webapp/hangoutamigosapp/js/hangoutamigos_controller.js
+++ src/main/webapp/hangoutamigosapp/js/hangoutamigos_controller.js
@@ -712,3 +712,14 @@ hangoutamigosapp.controller('restaurantController',
 	console.log('restaurantController end');
 
 });
+
+//Navbar controller
+hangoutamigosapp.controller('TestCtrl', function($scope, $location, $anchorScroll) {
+$scope.scrollTo = function(id) {
+    var old = $location.hash();
+    $location.hash(id);
+    $anchorScroll();
+    //reset to old to keep any additional routing logic from kicking in
+    $location.hash(old);
+};
+});
